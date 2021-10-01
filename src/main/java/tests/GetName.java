1d6d6c36a9e133338e05231edb615149f5ff4234
TestLeaf <babu@testleaf.com>
2021-10-02 02:15:35 +0530

    Simple API Automation scripts for WebApp

---
+++ src/main/java/tests/GetName.java
@@ -0,0 +1,29 @@
+package tests;
+
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Assert;
+
+
+public class GetName{
+	
+	
+
+	@Test()
+	public void login() {
+		
+		RestAssured.baseURI = "http://localhost:9001/api";
+		Response response = RestAssured.get("/Babu");
+		
+		Assert.assertEquals(true, response.getStatusCode() == 200);
+		Assert.assertEquals(true, response.getContentType().contains("text"));
+		
+		
+		
+	}
+	
+	
+}
