d5e53fd29b0a7bd20e459c1d8c4cd146eedcc605
vidya1926 <83778108+vidya1926@users.noreply.github.com>
2022-09-13 13:07:01 +0530

    Update GetName.java

--- src/main/java/tests/GetName.java
+++ src/main/java/tests/GetName.java
@@ -15,7 +15,7 @@ public class GetName{
 	@Test()
 	public void login() {
 		
-		RestAssured.baseURI = "http://localhost:9002/api";
+		RestAssured.baseURI = "http://localhost:7104/api";
 		Response response = RestAssured.get("/Babu");
 		
 		Assert.assertEquals(true, response.getStatusCode() == 200);
